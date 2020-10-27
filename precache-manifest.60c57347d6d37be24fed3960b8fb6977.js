cfd26aeebe93f203b563af31e02c61bf2faf7723
wxwdesign <wxwdesign@gmail.com>
2020-10-27 17:05:53 +0800

    deploy

---
+++ precache-manifest.60c57347d6d37be24fed3960b8fb6977.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ebb38c6a4ebc6671e2",
+    "url": "/color-generator/css/chunk-vendors.616b4a74.css"
+  },
+  {
+    "revision": "e7204d4ede76f699c556",
+    "url": "/color-generator/css/colors.76d976c5.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/color-generator/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/color-generator/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "c986f1b55d5a13031cf8d2d34283711b",
+    "url": "/color-generator/img/snapshot.png"
+  },
+  {
+    "revision": "79fc8c608aa82e5e6f7a96256fa8f375",
+    "url": "/color-generator/index.html"
+  },
+  {
+    "revision": "e7f251c463b1701b683c",
+    "url": "/color-generator/js/app.68ea4588.js"
+  },
+  {
+    "revision": "82ebb38c6a4ebc6671e2",
+    "url": "/color-generator/js/chunk-vendors.5220edc4.js"
+  },
+  {
+    "revision": "e7204d4ede76f699c556",
+    "url": "/color-generator/js/colors.5c2aae02.js"
+  },
+  {
+    "revision": "f89506906dfcc9849bc278af08d3c026",
+    "url": "/color-generator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/color-generator/robots.txt"
+  }
+]);
